290a434a7eb0a26838dba9cbbd5800870566df07
Martin Algesten <martin@algesten.se>
2014-01-07 12:03:06 +0530

    Started command line parsing

--- RabbiteerClient/Program.cs
+++ RabbiteerClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NDesk.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,71 @@ namespace Rabbiteer
         static void Main(string[] args)
         {
 
+            bool isBind = false;
+            bool isPublish = false;
 
+            var p = new OptionSet()
+            {
+                {"b|bind", v => isBind = true},
+                {"p|publish", v => isPublish = true}
+            };
+            p.Parse(args);
+
+            if (isBind)
+            {
+                doBind(args);
+            }
+            else if (isPublish)
+            {
+                doPublish(args);
+            }
+            else
+            {
+                Console.WriteLine("Usage: Rabbiteer.exe -b|-bind [options]");
+                Console.WriteLine("                     -p|-publish [options]");
+                Environment.Exit(-1);
+            }
 
         }
+
+
+        private static void doBind(string[] args)
+        {
+            BindCommand c = new BindCommand();
+            var p = new OptionSet()
+            {
+                {"q|queuename=", v => c.QueueName = v},
+                {"e|exchange=", v => c.Exchange = v},
+                {"r|routingkey=", v => c.RoutingKey = v},
+                {"o|outdir=", v => c.OutDir = v},
+            };
+            p.Parse(args);
+            if (!c.Validate())
+            {
+                Environment.Exit(-1);
+            }
+            (new Client()).AddCommand(c);
+        }
+
+        private static void doPublish(string[] args)
+        {
+            PublishCommand c = new PublishCommand();
+            char[] del = new char[]{'='};
+            var p = new OptionSet()
+            {
+                {"e|exchange=", v => c.Exchange = v},
+                {"r|routingkey=", v => c.RoutingKey = v},
+                {"h|header=", v => c.Headers.Add(v.Split(del, 2)[0], v.Split(del, 2)[1])},
+                {"f|file=", v => c.File = v},
+            };
+            p.Parse(args);
+            if (!c.Validate())
+            {
+                Environment.Exit(-1);
+            }
+            (new Client()).AddCommand(c);
+
+        }
+
     }
 }
--- RabbiteerSrvc/Command.cs
+++ RabbiteerSrvc/Command.cs
@@ -9,6 +9,43 @@ namespace Rabbiteer
     [Serializable]
     public abstract class Command
     {
+        public abstract bool Validate();
+    }
+
+
+    [Serializable]
+    public class BindCommand : Command
+    {
+        public string QueueName { get; set; }
+        public string Exchange { get; set; }
+        public string RoutingKey { get; set; }
+        public string OutDir { get; set; }
+
+        public override bool Validate()
+        {
+            bool ok = true;
+            if (QueueName == null)
+            {
+                Console.WriteLine("Missing QueueName");
+                ok = false;
+            }
+            if (Exchange == null)
+            {
+                Console.WriteLine("Missing Exchange");
+                ok = false;
+            }
+            if (RoutingKey == null)
+            {
+                Console.WriteLine("Missing RoutingKey");
+                ok = false;
+            }
+            if (OutDir == null)
+            {
+                Console.WriteLine("Missing OutDir");
+                ok = false;
+            }
+            return ok;
+        }
     }
 
     [Serializable]
@@ -18,15 +55,27 @@ namespace Rabbiteer
         public string RoutingKey { get; set; }
         public IDictionary<string, object> Headers = new Dictionary<string, object>();
         public string File { get; set; }
-    }
 
-    [Serializable]
-    public class BindCommand : Command
-    {
-        public string QueueName { get; set; }
-        public string Exchange { get; set; }
-        public string RoutingKey { get; set; }
-        public string OutDir { get; set; }
+        public override bool Validate()
+        {
+            bool ok = true;
+            if (Exchange == null)
+            {
+                Console.WriteLine("Missing Exchange");
+                ok = false;
+            }
+            if (RoutingKey == null)
+            {
+                Console.WriteLine("Missing RoutingKey");
+                ok = false;
+            }
+            if (File == null)
+            {
+                Console.WriteLine("Missing File");
+                ok = false;
+            }
+            return ok;
+        }
     }
 
 }
