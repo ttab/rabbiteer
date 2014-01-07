98f5f8a7e65e3016f071ff24b84b98fb728c6403
Martin Algesten <martin@algesten.se>
2014-01-07 12:09:51 +0530

    Fixed assembly info

--- RabbiteerClient/Properties/AssemblyInfo.cs
+++ RabbiteerClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Rabbiteer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Rabbiteer")]
+[assembly: AssemblyCopyright("Copyright © TT Nyhetsbyran 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- RabbiteerSrvc/Properties/AssemblyInfo.cs
+++ RabbiteerSrvc/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rabbiteer")]
+[assembly: AssemblyTitle("Rabbiteer Service")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rabbiteer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Rabbiteer Service")]
+[assembly: AssemblyCopyright("Copyright © TT Nyhetsbyran 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
